c5c8d29b8193c4b5036c9bfd8f54f7f2883dc2be
Jorge Santamaria <jorsebsant@gmail.com>
2020-10-29 22:11:39 -0500

    Testing Speech recognition with console logs

--- public/js/script.js
+++ public/js/script.js
@@ -7,3 +7,13 @@ recognition.interimResults = false;
 document.querySelector('button').addEventListener('click', () => {
   recognition.start();
 });
+
+recognition.addEventListener('result', (e) => {
+  let last = e.results.length - 1;
+  let text = e.results[last][0].transcript;
+
+  console.log('Confidence: ' + e.results[0][0].confidence);
+  console.log('text: ' + text);
+
+  // We will use the Socket.IO here later…
+});
