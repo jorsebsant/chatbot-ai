f9e364dbf297862a88d64b826358bff037ee9faf
Jorge Santamaria <jorsebsant@gmail.com>
2020-10-29 22:05:23 -0500

    Add SpeechRecognition api and listener

---
+++ public/js/script.js
@@ -0,0 +1,9 @@
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+recognition.lang = 'en-US';
+recognition.interimResults = false;
+
+document.querySelector('button').addEventListener('click', () => {
+  recognition.start();
+});
